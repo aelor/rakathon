48ae6656c994e0230ee7a7eccc97abf1655a8e65
Rituraj <rituraj@healthi.in>
2018-12-01 04:44:13 +0530

    fixed turbolinks and sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,17 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery-3.3.1
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require jquery-3.3.1
 //= require_tree .
 
 var activateLocateMe = function(){
     $('#locateMe').click(function(){
         console.log('located');
     })
-}
+};
+
 $(function(){
     activateLocateMe();
 });
