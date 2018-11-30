6bef0e32aad12fee684cd5cfe7def3f93b5c22aa
Rituraj <rituraj@healthi.in>
2018-12-01 04:00:30 +0530

    added markers for parking and car

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,10 @@
 class SearchController < ApplicationController
+  layout 'search'
+
   def index
   	lat = params[:lat] 
   	len = params[:lng] 
   	@search_records = Owner.within(5, :origin => [lat,len])
-  	puts @search_records.inspect
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -24,16 +24,6 @@ ActiveRecord::Schema.define(version: 2018_11_30_201120) do
     t.string "longitude"
   end
 
-  create_table "parkinglots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.text "address"
-    t.string "mobile"
-    t.string "no_of_parking"
-    t.string "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
