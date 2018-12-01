7f3fbd8b641d4a063fd018470b9ea1be16f034bc
Abhishek Kumar <abhi.kumar90@hotmail.com>
2018-12-01 05:42:26 +0530

    find distance

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,9 @@ class SearchController < ApplicationController
   layout 'search'
 
   def index
-  	lat = params[:lat] 
-  	len = params[:lng] 
-  	@search_records = Owner.within(5, :origin => [lat,len])
+  	@lat = params[:lat] 
+  	@len = params[:lng] 
+  	@search_records = Owner.within(5,:units => :kms,:origin => [@lat,@len], :formula => :sphere)
   end
 
 end
--- app/helpers/owners_helper.rb
+++ app/helpers/owners_helper.rb
@@ -1,2 +1,11 @@
 module OwnersHelper
+
+def get_distance(cur_location,lat, len)
+  obj1 = cur_location
+  obj2 = lat,len
+  distance = Owner.distance_between(obj1, obj2, :units => :kms, :formula => :sphere)
+  return distance.round(2)
+
+end
+
 end
